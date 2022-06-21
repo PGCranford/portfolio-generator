36a5cce86ab165950dc40c2226b864f7483d7289
PGCranford <PGCranford@gmail.com>
2022-06-21 15:29:53 -0400

    generate html

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [name, github] = profileDataArgs;
+// const generatePage = () => "Name: name, Github: patrickhub";
 
-const printProfileData = (profileDataArr) => {
-    //this..
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('================');
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
 
-};
 
-// printProfileData(profileDataArgs);
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+    if (err) throw err;
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
+
+
+
